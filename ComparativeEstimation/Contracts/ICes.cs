5a427847f6c3f930ded6b6a920b18c3396b5f895
Manuel Gutekunst <Manuel.Gutekunst@generic.de>
2017-07-11 16:49:17 +0200

    Kontrakte definiert

--- ComparativeEstimation/Contracts/ICes.cs
+++ ComparativeEstimation/Contracts/ICes.cs
@@ -1,9 +1,38 @@
-﻿namespace Contracts
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contracts
 {
     public interface ICes
     {
         void Anmeldung(string id);
 
+        void GewichteteVergleichspaare(IEnumerable<GewichtetesVergleichspaar> gewichteteVergleichspaare, Action ok, Action fehler);
 
+        Gesamtgewichtung Gesamtgewichtung();
+
+        void Storyliste(IEnumerable<string> stories);
+    }
+
+    public class Gesamtgewichtung
+    {
+        public string[] Stories { get; set; }
+
+        public int Anmeldungen { get; set; }
+
+        public int Gewichtungen { get; set; }
+    }
+
+    public class GewichtetesVergleichspaar
+    {
+        public string Id { get; set; }
+
+        public Selection Selection { get; set; }
+    }
+
+    public enum Selection
+    {
+        A,
+        B
     }
 }
