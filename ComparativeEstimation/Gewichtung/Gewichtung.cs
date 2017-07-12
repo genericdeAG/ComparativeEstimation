a094f23c34bde4f47cf47de86055f2e6b0d50e93
Manuel Gutekunst <Manuel.Gutekunst@generic.de>
2017-07-12 14:59:20 +0200

    Relationen erstellen

--- ComparativeEstimation/Gewichtung/Gewichtung.cs
+++ ComparativeEstimation/Gewichtung/Gewichtung.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Contracts;
 using Gewichtung.Model;
 
@@ -9,14 +10,24 @@ namespace Gewichtung
     {
         public void Gewichtung_berechne(IEnumerable<GewichtetesVergleichspaarDto> voting, IEnumerable<Vergleichspaar> vergleichspaare, Action<Contracts.Gewichtung> ok, Action fehler)
         {
-            var relationen = this.Relationen_erstellen(voting, vergleichspaare);
+            var relationen = Relationen_erstellen(voting, vergleichspaare);
             var graph = GraphGenerator.Graph_erstellen(relationen);
             GraphGenerator.Sortieren(graph, ok, fehler);
         }
 
-        private IEnumerable<IndexTupel> Relationen_erstellen(IEnumerable<GewichtetesVergleichspaarDto> voting, IEnumerable<Vergleichspaar> vergleichspaare)
+        internal static IEnumerable<IndexTupel> Relationen_erstellen(IEnumerable<GewichtetesVergleichspaarDto> voting, IEnumerable<Vergleichspaar> vergleichspaare)
         {
-            throw new NotImplementedException();
+            var paare = vergleichspaare.ToList();
+            return voting.Select(v => ErstelleTupel(v, paare.First(p => p.Id == v.Id)));
+        }
+
+        private static IndexTupel ErstelleTupel(GewichtetesVergleichspaarDto v, Vergleichspaar paar)
+        {
+            return new IndexTupel
+            {
+                Wichtiger = v.Selektion == Selektion.A ? paar.IndexA : paar.IndexB,
+                Weniger = v.Selektion == Selektion.A ? paar.IndexB : paar.IndexA
+            };
         }
 
         public Contracts.Gewichtung Gesamtgewichtung_berechne(IEnumerable<Contracts.Gewichtung> gewichtungen)
--- ComparativeEstimation/Gewichtung/Tests/GewichtungTests.cs
+++ ComparativeEstimation/Gewichtung/Tests/GewichtungTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Contracts;
+using Gewichtung.Model;
 using Shouldly;
 using Xunit;
 
@@ -30,7 +32,7 @@ namespace Gewichtung.Tests
             sut.Gewichtung_berechne(
                 voting,
                 paare,
-                g => g.StoryIndizes.ShouldBe(new [] {2,0,1}),
+                g => g.StoryIndizes.ShouldBe(new[] { 2, 0, 1 }),
                 () => { throw new Exception(); }
             );
         }
@@ -56,11 +58,65 @@ namespace Gewichtung.Tests
             sut.Gewichtung_berechne(
                 voting,
                 paare,
-                g => throw new Exception("kein Fehler"),
+                g => { throw new Exception("kein Fehler"); },
                 () => Assert.True(true)
             );
         }
 
 
+        [Fact]
+        public void Relationen()
+        {
+            IEnumerable<GewichtetesVergleichspaarDto> voting = new List<GewichtetesVergleichspaarDto>
+                {
+                    new GewichtetesVergleichspaarDto
+                    {
+                        Id = "0",
+                        Selektion = Selektion.A
+                    },
+                    new GewichtetesVergleichspaarDto
+                    {
+                        Id = "1",
+                        Selektion = Selektion.B
+                    },
+                    new GewichtetesVergleichspaarDto
+                    {
+                        Id = "2",
+                        Selektion = Selektion.B
+                    },
+                };
+
+            IEnumerable<Vergleichspaar> vergleichspaare = new List<Vergleichspaar>
+            {
+                new Vergleichspaar
+                {
+                    Id = "0",
+                    IndexA = 0,
+                    IndexB = 1,
+                },
+                new Vergleichspaar
+                {
+                    Id = "1",
+                    IndexA = 0,
+                    IndexB = 2,
+                },
+                new Vergleichspaar
+                {
+                    Id = "2",
+                    IndexA = 1,
+                    IndexB = 2,
+                },
+            };
+            var result = Gewichtung.Relationen_erstellen(voting, vergleichspaare).ToList();
+            AssertIndize(result[0], 0, 1);
+            AssertIndize(result[1], 2, 0);
+            AssertIndize(result[2], 2, 1);
+        }
+
+        private void AssertIndize(IndexTupel tuple, int wichtiger, int weniger)
+        {
+            tuple.Weniger.ShouldBe(weniger);
+            tuple.Wichtiger.ShouldBe(wichtiger);
+        }
     }
 }
