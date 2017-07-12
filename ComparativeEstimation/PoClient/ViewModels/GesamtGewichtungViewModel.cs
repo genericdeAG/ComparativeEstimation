7eaf7f40819c4311ab3bad3ff3fd6831dfbf390d
Matthias Nistl <matthias.nistl@gmx.de>
2017-07-12 13:15:07 +0200

    View+ViewModel Gesamtgewichtung implementiert

--- ComparativeEstimation/PoClient/ViewModels/GesamtGewichtungViewModel.cs
+++ ComparativeEstimation/PoClient/ViewModels/GesamtGewichtungViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Timers;
+using Contracts;
+using Prism.Mvvm;
 
 namespace PoClient.ViewModels
 {
-    public class GesamtGewichtungViewModel
+    public class GesamtGewichtungViewModel : BindableBase
     {
+        private ICes restProvider;
+
+        private string votingCounterText;
+
+        public string VotingCounterText
+        {
+            get => votingCounterText;
+            set => SetProperty(ref votingCounterText, value);
+        }
+
+        public ObservableCollection<string> Gesamtgewichtung { get; } = new ObservableCollection<string>();
+
+        public GesamtGewichtungViewModel()
+        {
+            restProvider = App.Resolve<ICes>();
+            var timer1 = new Timer();
+            timer1.Elapsed += (s, e) =>
+            {
+                Refresh();
+            };
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        public void Refresh()
+        {
+            var gesamtgewichtung = restProvider.Gesamtgewichtung;
+            if (gesamtgewichtung == null)
+            {
+                gesamtgewichtung = new GesamtgewichtungDto();
+            }
+
+
+            VotingCounterText = $"{gesamtgewichtung.Gewichtungen} / {gesamtgewichtung.Anmeldungen}";
+            Gesamtgewichtung.Clear();
+            Gesamtgewichtung.AddRange(gesamtgewichtung.Stories);
+        }
     }
 }
