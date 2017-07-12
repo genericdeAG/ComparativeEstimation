b060b1a97f8f3da1e2d309545074a2682f0ded83
Matthias Nistl <matthias.nistl@gmx.de>
2017-07-12 16:52:00 +0200

    URL-Setting für POClient hinzugefügt

--- ComparativeEstimation/PoClient/Bootstrapper.cs
+++ ComparativeEstimation/PoClient/Bootstrapper.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
+using CeServerProvider;
 using Contracts;
 using Microsoft.Practices.Unity;
+using PoClient.Properties;
 using PoClient.Views;
 using Prism.Unity;
 
@@ -12,7 +14,14 @@ namespace PoClient
         {
             base.ConfigureContainer();
 
-            this.Container.RegisterInstance<ICes>(new DummyProvider());
+            var url = Settings.Default.ServerUrl;
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("Keine URL hinterlegt!", "Fehler");
+                Application.Current.Shutdown();
+            }
+
+            this.Container.RegisterInstance<ICes>(new RestProvider(url));
         }
 
         protected override DependencyObject CreateShell()
