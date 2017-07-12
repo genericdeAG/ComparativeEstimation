135e002576c26f9d218ab66d5a0725dc4cba20ed
Ralf Westphal <info@ralfw.de>
2017-07-12 20:13:51 +0200

    CE Server komponentenorientiert

---
+++ ralfw/ce.domain/ce.domain/RequestHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ce.contracts;
+
+namespace ce.domain
+{
+    public class RequestHandler : ICes
+    {
+        internal class State
+        {
+            public string[] Stories = new string[0];
+            public Vergleichspaar[] Vergleichspaare = new Vergleichspaar[0];
+            public HashSet<string> Anmeldungen = new HashSet<string>();
+            public List<Gewichtung> Gewichtungen = new List<Gewichtung>();
+        }
+
+        private IGewichtung gewichter;
+        private State state;
+
+
+        public RequestHandler(IGewichtung gewichter) : this(gewichter, new State()) { }
+        internal RequestHandler(IGewichtung gewichter, State state)
+        {
+            this.gewichter = gewichter;
+            this.state = state;
+        }
+
+
+        public void Sprint_lösche()
+        {
+            this.state.Stories = new string[0];
+            this.state.Anmeldungen = new HashSet<string>();
+            this.state.Gewichtungen = new List<Gewichtung>();
+            this.state.Vergleichspaare = new Vergleichspaar[0];
+        }
+
+
+        public void Sprint_âlege(IEnumerable<string> stories)
+        {
+            this.Sprint_lösche();
+            this.state.Stories = stories.ToArray();
+            this.state.Vergleichspaare = Vergleichspaare_berechnen(this.state.Stories.Length).ToArray();
+        }
+
+        internal static IEnumerable<Vergleichspaar> Vergleichspaare_berechnen(int numberOfStories)
+        {
+            for (var indexA = 0; indexA < numberOfStories; indexA++)
+                for (var indexB = indexA + 1; indexB < numberOfStories; indexB++)
+                    yield return new Vergleichspaar
+                    {
+                        Id = (100 * indexA + indexB).ToString(),
+                        IndexA = indexA,
+                        IndexB = indexB
+                    };
+        }
+
+
+        public void Âmelde(string id)
+        {
+            if (this.state.Anmeldungen == null) this.state.Anmeldungen = new HashSet<string>();
+            this.state.Anmeldungen.Add(id);
+        }
+
+
+        public IEnumerable<VergleichspaarDto> Vergleichspaare
+            => this.state.Vergleichspaare.Select(vp => new VergleichspaarDto
+            {
+                Id = vp.Id,
+                A = this.state.Stories[vp.IndexA],
+                B = this.state.Stories[vp.IndexB]
+            });
+
+
+        public void Gewichtung_regischtriere(IEnumerable<GewichtetesVergleichspaarDto> voting, Action ok, Action fehler)
+        {
+            this.gewichter.Gewichtung_berechne(voting, this.state.Vergleichspaare,
+               gewichtung =>
+               {
+                   //TODO: Registrierung idempotent machen bzw. User zuordnen; sonst kann es dazu kommen, dass es mehr Gewichtungen gibt als Anmeldungen
+                   this.state.Gewichtungen.Add(gewichtung);
+                   ok();
+               },
+               fehler);
+        }
+
+
+        public GesamtgewichtungDto Gesamtgewichtung
+        {
+            get
+            {
+                var gesamtgewichung = this.gewichter.Gesamtgewichtung_berechne(this.state.Gewichtungen);
+                var stories = Stories_zur_Gewichtung(gesamtgewichung, this.state.Stories);
+                return new GesamtgewichtungDto
+                {
+                    Stories = stories.ToArray(),
+                    Anmeldungen = this.state.Anmeldungen.Count,
+                    Gewichtungen = this.state.Gewichtungen.Count()
+                };
+            }
+        }
+
+        internal static IEnumerable<string> Stories_zur_Gewichtung(Gewichtung gewichtung, string[] stories)
+            => gewichtung.StoryIndizes.Select(si => stories[si]);
+    }
+}
---
+++ ralfw/ce.server/ce.server.adapters/RESTPortal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using servicehost.contract;
+using ce.contracts;
+
+namespace ce.server.adapters
+{
+    public static class ApplicationState
+    {
+        public static ICes RequestHandler;
+    }
+
+
+    [Service]
+    public class RESTPortal
+    {
+        [EntryPoint(HttpMethods.Post, "/Anmeldungen", InputSources.Querystring)]
+        public string Anmeldungen(string id)
+        {
+            Console.WriteLine($"  POST.Anmeldungen({id})");
+
+            ApplicationState.RequestHandler.Âmelde(id);
+            return "";
+        }
+
+
+        [EntryPoint(HttpMethods.Get, "/Vergleichspaare", InputSources.Querystring)]
+        public string Vergleichspaare(string _)
+        {
+            Console.WriteLine($"  GET.Vergleichspaare()");
+
+            var vergleichspaare = ApplicationState.RequestHandler.Vergleichspaare;
+
+            var json = new JavaScriptSerializer();
+            return json.Serialize(vergleichspaare);
+        }
+
+
+        [EntryPoint(HttpMethods.Post, "/Voting", InputSources.Payload)]
+        public string Voting(string jsonVoting)
+        {
+            Console.WriteLine($"  POST.Voting({jsonVoting})");
+
+            var json = new JavaScriptSerializer();
+            GewichtetesVergleichspaarDto[] voting = json.Deserialize<GewichtetesVergleichspaarDto[]>(jsonVoting);
+
+            var status = "";
+            ApplicationState.RequestHandler.Gewichtung_regischtriere(voting,
+                 () => status = "200",
+                 () => status = "422");
+
+            return status;
+        }
+
+
+        [EntryPoint(HttpMethods.Delete, "/Storyliste", InputSources.Querystring)]
+        public string Reset(string _)
+        {
+            Console.WriteLine($"  DEL.Storyliste");
+
+            ApplicationState.RequestHandler.Sprint_lösche();
+            return "";
+        }
+
+
+        [EntryPoint(HttpMethods.Post, "/Sprint", InputSources.Payload)]
+        public string Sprint(string jsonStories)
+        {
+            Console.WriteLine($"  POST.Sprint({jsonStories})");
+
+            var json = new JavaScriptSerializer();
+            string[] stories = json.Deserialize<string[]>(jsonStories);
+
+            ApplicationState.RequestHandler.Sprint_âlege(stories);
+
+            return "";
+        }
+
+
+        [EntryPoint(HttpMethods.Get, "/Gesamtgewichtung", InputSources.Querystring)]
+        public string Gesamtgewichtung(string _)
+        {
+            Console.WriteLine($"  GET.Gesamtgewichtung");
+
+            var gesamtgewichtung = ApplicationState.RequestHandler.Gesamtgewichtung;
+
+            var json = new JavaScriptSerializer();
+            return json.Serialize(gesamtgewichtung);
+        }
+    }   
+}
