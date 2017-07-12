0ddb7f1a1439625612d5482390a0a57cce6e3744
Ralf Westphal <info@ralfw.de>
2017-07-12 11:18:12 +0200

    CeDomain begonnen

---
+++ ComparativeEstimation/CeDomain/RequestHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Contracts;
+
+namespace CeDomain
+{
+    internal class RequestHandlerState {
+        public IEnumerable<Vergleichspaar> vergleichspaare;
+        public HashSet<string> anmeldungen;
+        public IEnumerable<Gewichtung> gewichtungen;
+    }
+
+    public class RequestHandler : ICes
+    {
+        private IGewichtung gewichter;
+
+        private string[] stories;
+        private RequestHandlerState state;
+
+
+        public RequestHandler(IGewichtung gewichter) : this(gewichter, new RequestHandlerState()) {}
+        internal RequestHandler(IGewichtung gewichter, RequestHandlerState state) {
+            this.gewichter = gewichter;
+            this.state = state;
+        }
+
+
+        public GesamtgewichtungDto Gesamtgewichtung => throw new NotImplementedException();
+
+        public IEnumerable<VergleichspaarDto> Vergleichspaare => throw new NotImplementedException();
+
+        public void Gewichtung_regischtriere(IEnumerable<GewichtetesVergleichspaarDto> voting, Action ok, Action fehler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sprint_lösche()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sprint_âlege(IEnumerable<string> stories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Âmelde(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ComparativeEstimation/CeDomain/test_RequestHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CeDomain
+{
+    [TestFixture]
+    public class test_RequestHandler
+    {
+        [Test]
+        public void Anmelden()
+        {
+            var state = new RequestHandlerState();
+            var sut = new RequestHandler(null, state);
+
+            sut.Âmelde("1");
+            Assert.AreEqual(1, state.anmeldungen.Count);
+
+            sut.Âmelde("2");
+            Assert.AreEqual(2, state.anmeldungen.Count);
+        }
+
+        [Test]
+        public void Anmelden_idempotent()
+        {
+            var state = new RequestHandlerState();
+            var sut = new RequestHandler(null, state);
+
+            sut.Âmelde("1");
+            Assert.AreEqual(1, state.anmeldungen.Count);
+
+            sut.Âmelde("1");
+            Assert.AreEqual(1, state.anmeldungen.Count);
+        }
+    }
+}
