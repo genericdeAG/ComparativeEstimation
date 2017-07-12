6a554a35cda14cba4643aea59f2461c47be2217b
Ralf Westphal <info@ralfw.de>
2017-07-12 16:39:56 +0200

    Tests auf xunit umgestellt
    privates projekt für server auf .net fx angelegt

---
+++ ralfw/CeWithdotnetFx/Gewichtung/Gewichtungen.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Contracts;
+
+namespace Gewichtung
+{
+    public class Gewichtungen : IGewichtung
+    {
+        public Contracts.Gewichtung Gesamtgewichtung_berechne(IEnumerable<Contracts.Gewichtung> gewichtungen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Gewichtung_berechne(IEnumerable<GewichtetesVergleichspaarDto> voting, IEnumerable<Vergleichspaar> vergleichspaare, Action<Contracts.Gewichtung> ok, Action fehler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
