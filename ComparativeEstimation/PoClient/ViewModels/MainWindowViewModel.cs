2bf5e06de811db02d74461d7c711ede662ea6e9e
Matthias Nistl <matthias.nistl@gmx.de>
2017-07-12 11:48:38 +0200

    MainWindow vorbereitet

---
+++ ComparativeEstimation/PoClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using Contracts;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace PoClient.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private ICes restProvider;
+
+        public ObservableCollection<string> Stories { get; } = new ObservableCollection<string>();
+
+        private string currentStory;
+
+        public string CurrentStory
+        {
+            get => currentStory;
+            set => SetProperty(ref currentStory, value);
+        }
+        
+        public DelegateCommand AddStoryCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            AddStoryCommand = new DelegateCommand(AddStory);
+        }
+
+        public void AddStory()
+        {
+            Stories.Add(CurrentStory);
+        }
+
+        public void Reset()
+        {
+            restProvider.Sprint_lösche();
+        }
+    }
+}
---
+++ ComparativeEstimation/PoClient/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using PoClient.ViewModels;
+
+namespace PoClient.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
