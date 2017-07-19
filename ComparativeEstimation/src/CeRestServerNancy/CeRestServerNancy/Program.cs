0a53373ceac913eec2ce828d603f7030a9e8e169
Ralf Westphal <info@ralfw.de>
2017-07-19 18:58:33 +0200

    Zielverzeichnis für Build korrigiert. /bin liegt auf einer Ebene mit /src. Nancy Server Sln angelegt. build Sln mit Nancy Server angelegt.

---
+++ ComparativeEstimation/src/CeRestServerNancy/CeRestServerNancy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CeRestServerNancy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
