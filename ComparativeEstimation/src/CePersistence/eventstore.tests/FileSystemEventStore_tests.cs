33be7bbb7f04353770512ad94677e3d8fb21ef73
Ralf Westphal <info@ralfw.de>
2017-07-29 15:26:13 +0200

    Persistenz begonnen

---
+++ ComparativeEstimation/src/CePersistence/eventstore.tests/FileSystemEventStore_tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace eventstore.tests
+{
+    [TestFixture()]
+    public class FileSystemEventStore_tests
+    {
+        [SetUp()]
+        public void Setup() {
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        }
+
+        [Test()]
+        public void Create()
+        {
+            const string eventStoreFolderPath = "ESCreationTest";
+            if (Directory.Exists(eventStoreFolderPath)) Directory.Delete(eventStoreFolderPath, true);
+
+            var sut = new FilesystemEventStore(eventStoreFolderPath);
+            Assert.IsTrue(Directory.Exists(eventStoreFolderPath));
+
+            var sut2 = new FilesystemEventStore(eventStoreFolderPath);
+            Assert.IsTrue(Directory.Exists(eventStoreFolderPath));
+        }
+    }
+}
---
+++ ComparativeEstimation/src/CePersistence/eventstore/FilesystemEventStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace eventstore
+{
+    public class Event {
+        public string Index;
+        public string Name;
+        public string ContextId;
+        public string Payload;
+        public DateTime Timestamp;
+    }
+
+    public interface IEventStore {
+        void Append(Event e);
+        void Append(IEnumerable<Event> events);
+
+        IEnumerable<Event> Replay();
+        IEnumerable<Event> Replay(string contextId);
+
+        event Action<IEnumerable<Event>> OnAppended;
+    }
+
+
+    public class FilesystemEventStore : IEventStore
+    {
+        readonly string folderPath;
+
+        public FilesystemEventStore() : this("EventStore") {}
+        public FilesystemEventStore(string folderPath) {
+            this.folderPath = folderPath;
+            Directory.CreateDirectory(this.folderPath);
+        }
+
+        public void Append(Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Append(IEnumerable<Event> events)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Event> Replay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Event> Replay(string contextId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<IEnumerable<Event>> OnAppended;
+    }
+}
