27890b22e06154a005761dc9f8854f87f629ac7e
Ralf Westphal <info@ralfw.de>
2017-07-30 10:23:27 +0200

    Eventstore schreibt Events

--- ComparativeEstimation/src/CePersistence/eventstore.tests/FileSystemEventStore_tests.cs
+++ ComparativeEstimation/src/CePersistence/eventstore.tests/FileSystemEventStore_tests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace eventstore.tests
 {
@@ -24,5 +26,80 @@ namespace eventstore.tests
             var sut2 = new FilesystemEventStore(eventStoreFolderPath);
             Assert.IsTrue(Directory.Exists(eventStoreFolderPath));
         }
+
+        [Test]
+        public void Append() {
+            const string eventStoreFolderPath = "ESAppendTest";
+            var e = new Event
+            {
+                ContextId = "ctx1",
+                Name = "eventname",
+                Payload = "payload1\npayload2"
+            };
+            var sut = new FilesystemEventStore(eventStoreFolderPath);
+            sut.OnAppended += _ => { };
+
+            var seqnum = sut.Append(e);
+
+            var eventText = File.ReadAllLines(Path.Combine(eventStoreFolderPath, e.ContextId, seqnum + ".txt"));
+            Assert.AreEqual(4, eventText.Length);
+            Assert.AreEqual("eventname", eventText[0]);
+            Assert.IsTrue(eventText[1].IndexOf("2017") >= 0);
+            Assert.AreEqual("payload1", eventText[2]);
+            Assert.AreEqual("payload2", eventText[3]);
+        }
+
+        [Test]
+        public void Append_fires_events()
+        {
+            const string eventStoreFolderPath = "ESAppendTest";
+            var e = new Event
+            {
+                ContextId = "ctx1",
+                Name = "eventname",
+                Payload = "payload1\npayload2"
+            };
+            var sut = new FilesystemEventStore(eventStoreFolderPath);
+
+            Event result = null;
+            sut.OnAppended += events => result = events.First();
+
+            var seqnum = sut.Append(e);
+
+            Assert.AreEqual(seqnum, result.SequenceNumber);
+            Assert.AreEqual("ctx1", result.ContextId);
+            Assert.AreEqual("eventname", result.Name);
+        }
+
+        [Test]
+        public void Append_several()
+        {
+            const string eventStoreFolderPath = "ESAppendTest";
+            if (Directory.Exists(eventStoreFolderPath)) Directory.Delete(eventStoreFolderPath, true);
+
+            var e1 = new Event {
+                ContextId = "ctx1",
+                Name = "eventname",
+                Payload = "payload1\npayload2"
+            };
+            var e2 = new Event {
+                ContextId = "ctx1",
+                Name = "eventname2",
+                Payload = "payload"
+            };
+            var sut = new FilesystemEventStore(eventStoreFolderPath);
+            Event[] result = null;
+            sut.OnAppended += events => result = events.ToArray();
+
+            var seqnum = sut.Append(new[]{e1, e2});
+
+            var contextFolderPath = Path.Combine(eventStoreFolderPath, e1.ContextId);
+            var filenames = Directory.GetFiles(contextFolderPath, "*.txt");
+            Assert.AreEqual(2, filenames.Length);
+
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual("eventname2", result[1].Name);
+        }
+
     }
 }
--- ComparativeEstimation/src/CePersistence/eventstore/FilesystemEventStore.cs
+++ ComparativeEstimation/src/CePersistence/eventstore/FilesystemEventStore.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace eventstore
 {
     public class Event {
-        public string Index;
+        public string SequenceNumber;
         public string Name;
         public string ContextId;
         public string Payload;
-        public DateTime Timestamp;
+        public DateTime Timestamp = DateTime.Now;
     }
 
     public interface IEventStore {
-        void Append(Event e);
-        void Append(IEnumerable<Event> events);
+        string Append(Event e);
+        string[] Append(IEnumerable<Event> events);
 
         IEnumerable<Event> Replay();
         IEnumerable<Event> Replay(string contextId);
@@ -33,15 +35,47 @@ namespace eventstore
             Directory.CreateDirectory(this.folderPath);
         }
 
-        public void Append(Event e)
-        {
-            throw new NotImplementedException();
+
+        public string[] Append(IEnumerable<Event> events) {
+            events.ToList().ForEach(Write);
+            OnAppended(events);
+            return events.Select(e => e.SequenceNumber).ToArray();
         }
 
-        public void Append(IEnumerable<Event> events)
-        {
-            throw new NotImplementedException();
+        public string Append(Event e) {
+            Write(e);
+            OnAppended(new[]{e});
+            return e.SequenceNumber;
         }
+
+        private void Write(Event e) {
+            e.SequenceNumber = Next_sequence_number();
+            var contextPath = Ensure_context_folder(e.ContextId);
+            var eventText = Serialize(e);
+            Write(contextPath, e.SequenceNumber, eventText);
+        }
+
+        private void Write(string contextPath, string sequenceNumber, string eventText) {
+            var eventPath = Path.Combine(contextPath, sequenceNumber + ".txt");
+            File.WriteAllText(eventPath, eventText);
+        }
+
+        private string Ensure_context_folder(string contextId) {
+            var contextPath = Path.Combine(this.folderPath, contextId);
+            Directory.CreateDirectory(contextPath);
+            return contextPath;
+        }
+
+        private string Serialize(Event e) {
+            var eventText = new StringWriter();
+            eventText.WriteLine(e.Name);
+            eventText.WriteLine("{0:O}", e.Timestamp);
+            eventText.WriteLine(e.Payload);
+            return eventText.ToString();
+        }
+
+
+
 
         public IEnumerable<Event> Replay()
         {
@@ -52,6 +86,13 @@ namespace eventstore
         {
             throw new NotImplementedException();
         }
+
+
+        private string Next_sequence_number() {
+            var t = DateTime.Now.Ticks;
+            return string.Format("{0:00000000000000000000}", t);
+        }
+
 
         public event Action<IEnumerable<Event>> OnAppended;
     }
