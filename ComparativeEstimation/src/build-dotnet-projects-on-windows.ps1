1542a4fde52cfb639e455d385c96ed8aaebd4012
Bastian Knochel <bastian.knochel@generic.de>
2017-08-13 10:56:59 +0200

    build_all sln hinzugefügt und Nancy projekt gefixt

--- ComparativeEstimation/src/build-dotnet-projects-on-windows.ps1
+++
@@ -1,6 +0,0 @@
-$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-& $msbuild ".\CeContracts\CeContracts.sln" 
-& $msbuild ".\CeWeighting\CeWeighting.sln"
-& $msbuild ".\CePersistence\CePersistence.sln"
-& $msbuild ".\CeDomain\CeDomain.sln"
-& $msbuild ".\CeRestServerNancy\CeRestServerNancy.sln"
